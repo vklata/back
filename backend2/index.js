d7b15923a503baafa6d7a570cb7b9a890c9fc9e5
vklata <151921976+vklata@users.noreply.github.com>
2024-07-16 21:53:55 +0530

    Update index.js

--- backend2/index.js
+++ backend2/index.js
@@ -6,17 +6,25 @@ import categoryRoutes from './Routes/categoryRoute.js'
 import productRoutes from './Routes/productRoutes.js'
 import paymentRoutes from './Routes/paymentRoutes.js'
 connectDB();
-import cors from 'cors'
+// import cors from 'cors'
 import mongoose from 'mongoose'
 const app = express();
 // app.use(cors());
 app.use(express.json());
-app.use(cors({
-  // origin: 'http://localhost:5173'
-  origin:["https://shoping-kzk7.vercel.app"],
-  methods:["POST","GET"],
-  credentials:true
-}));
+// app.use(cors({
+//   // origin: 'http://localhost:5173'
+//   origin:["https://shoping-kzk7.vercel.app"],
+//   methods:["POST","GET"],
+//   credentials:true
+// }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://shoping-ejxc.vercel.app");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 mongoose.connect('mongodb+srv://vishnukaushik173:kklata123@cluster0.wzxhdvu.mongodb.net/orderquick?retryWrites=true&w=majority&appName=Cluster0');
 app.use('/api',authRoutes)
 app.use('/api',categoryRoutes)
