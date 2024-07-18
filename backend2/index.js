76fd5a6eed39935d1abf4fe3790a09f1820d720c
vklata <151921976+vklata@users.noreply.github.com>
2024-07-18 17:08:22 +0530

    Update index.js

--- backend2/index.js
+++ backend2/index.js
@@ -9,22 +9,22 @@ import paymentRoutes from './Routes/paymentRoutes.js'
 import cors from 'cors'
 import mongoose from 'mongoose'
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
-app.use(cors({
-  // origin: 'https://shoping-kzk7-deoq12urv-vishnulatas-projects.vercel.app',
-  origin:["https://shoping-kzk7-deoq12urv-vishnulatas-projects.vercel.app"],
-  methods:["POST","GET"],
-  credentials:true
-}));
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "https://shoping-kzk7-deoq12urv-vishnulatas-projects.vercel.app");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+// app.use(cors({
+//   // origin: 'https://shoping-kzk7-deoq12urv-vishnulatas-projects.vercel.app',
+//   origin:["https://shoping-kzk7-deoq12urv-vishnulatas-projects.vercel.app"],
+//   methods:["POST","GET"],
+//   credentials:true
+// }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://shoping-kzk7-deoq12urv-vishnulatas-projects.vercel.app");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 mongoose.connect('mongodb+srv://vishnukaushik173:kklata123@cluster0.wzxhdvu.mongodb.net/orderquick?retryWrites=true&w=majority&appName=Cluster0');
 app.use('/api',authRoutes)
 app.use('/api',categoryRoutes)
